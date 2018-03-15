2234ec9ecab0a9d5a136179ea5d9842295d8215e
Fragzowns <37031279+Fragzowns@users.noreply.github.com>
2018-03-15 18:44:13 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,5 +17,10 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'Peep') {
+    	message.reply('Our goose god');
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
